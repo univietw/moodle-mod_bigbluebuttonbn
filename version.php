7824a84bc7b6264c4b98dd9c37bfe141ef3012e0
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2020-07-10 11:49:28 +0200

    add guest access feature

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019101005;
+$plugin->version = 2019101006;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
